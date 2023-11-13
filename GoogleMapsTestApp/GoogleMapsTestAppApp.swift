4fdcb4c2647b98ed26021b578f72506039711ccf
Moshentsev Nikita <trane0909@bk.ru>
2023-11-13 22:11:11 +0300

    add Google Maps and UI

--- GoogleMapsTestApp/GoogleMapsTestAppApp.swift
+++ GoogleMapsTestApp/GoogleMapsTestAppApp.swift
@@ -5,13 +5,17 @@
 //  Created by Никита Мошенцев on 13.11.2023.
 //
 
+import GoogleMaps
 import SwiftUI
 
 @main
 struct GoogleMapsTestAppApp: App {
+    init () {
+        GMSServices.provideAPIKey("AIzaSyBCzZZicSBHz6H9p-6-U3Jdz8OATYFhFk4")
+    }
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MapView()
         }
     }
 }
