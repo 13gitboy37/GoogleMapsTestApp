272973ce3b04a1df06c6fee46c4d9cefa3a660ad
Moshentsev Nikita <trane0909@bk.ru>
2023-11-13 17:56:32 +0300

    Create project

---
+++ GoogleMapsTestApp/GoogleMapsTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleMapsTestAppApp.swift
+//  GoogleMapsTestApp
+//
+//  Created by Никита Мошенцев on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct GoogleMapsTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
